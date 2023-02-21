4c068a6ba80e793f6e3e187bf16dcb1a92435696
Lorenzo Young <lyoun306@mtroyal.ca>
2023-02-21 08:46:05 -0700

    added the fetch data

--- react-asg01/src/App.js
+++ react-asg01/src/App.js
@@ -1,7 +1,38 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useEffect, useState } from "react";
+import { trackPromise } from 'react-promise-tracker';
 
 function App() {
+
+
+  const [movies, setMovies] = useState([]);
+  trackPromise(
+    useEffect(() => {
+      const url = 'https://www.randyconnolly.com/funwebdev/3rd/api/movie/movies-brief.php?limit=200';
+      if (movies.length <= 0) {
+        // First retrieve data from localStorage
+        const isLocalData = localStorage.getItem("key")
+        // Is it There?
+        if (isLocalData) {
+          console.log("Retrieving data from local storage")
+          const data = JSON.parse(isLocalData);
+          console.log(data)
+          setMovies(data);
+        } else {
+          console.log('Local storage empty. Fetching data remotely')
+          fetch(url)
+            .then(resp => resp.json())
+            .then(data => {
+              localStorage.setItem("key", JSON.stringify(data));
+              setMovies(data);
+            });
+        }
+      }
+    }, [movies])
+  );
+
   return (
     <div className="App">
       <header className="App-header">
--- react-asg01/src/index.js
+++ react-asg01/src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import LoadingIndicator from './components/LoadingIndicator.js'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+      <LoadingIndicator />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
